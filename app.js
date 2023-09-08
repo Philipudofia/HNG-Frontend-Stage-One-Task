ebc19976220eb9e9534105dcb4deba126a51fb76
Philipudofia <98493199+Philipudofia@users.noreply.github.com>
2023-09-08 12:52:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+function setDate() {
+    let day = document.querySelector('.currentDay')
+    let time = document.querySelector('.currentTime')
+    const currentDate = new Date()
+    const currentDay = currentDate.getUTCDay()
+    const currentMonth = currentDate.getMonth()
+    console.log(currentMonth)
+    const currentYear = currentDate.getFullYear()
+    const day2 = currentDate.getUTCDate()
+    const daysOfWeek =[
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ]
+    const dayOfTheWeek = daysOfWeek[currentDay]
+    let currentHour = String(currentDate.getUTCHours())
+    let currentMinute = String(currentDate.getUTCMinutes())
+    const seconds = currentDate.getUTCSeconds()
+    day.textContent = dayOfTheWeek
+    if (currentHour.length < 2) {
+        currentHour = `0` + currentHour
+    }
+    if (currentMinute.length < 2) {
+        currentMinute = `0` + currentMinute
+    }
+    time.textContent = `${day2}-${currentMonth + 1}-${currentYear} 
+    ${currentHour}:${currentMinute}:${seconds}`
+}
+
+setInterval(setDate, 1000);
+
+const image = document.querySelector('.displayImage')
+image.src = 'philipUdofia.png'
+
+const track = document.querySelector('.myTrack')
+track.textContent = 'FrontEnd'
